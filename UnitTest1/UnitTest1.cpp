adcf0f4aafc2d7fb841fa5e7b86ecb4916f507cd
Tylko-jedno <diogkek@gmail.com>
2024-10-28 17:25:51 +0200

    first master commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../sum.h"
+#include "../dod.h"
+
+using namespace nsSum;
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double s = 0;
+			sum();
+			Assert::AreEqual(s, 1.0, 0.000001);
+		}
+	};
+}
